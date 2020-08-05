4d255835016b97b0d41cd2367b6e5ae04f3ecd78
SegaraRai <SegaraRai@users.noreply.github.com>
2020-08-05 19:16:54 +0900

    stdafx.hppのinclude順を修正

--- tlg-wic-codec/stdafx.hpp
+++ tlg-wic-codec/stdafx.hpp
@@ -1,11 +1,13 @@
-#pragma once
+﻿#pragma once
 
 #include <vector>
 
 #define NOMINMAX
+#include <Windows.h>
+#include <wincodec.h>
+
 #include <propkey.h>
 #include <propsys.h>
 #include <propvarutil.h>
+
 #include <tchar.h>
-#include <wincodec.h>
-#include <windows.h>
