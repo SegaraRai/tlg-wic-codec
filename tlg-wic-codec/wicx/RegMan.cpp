06f51c04eae8935230d5ee16b6e3283a3a98414c
SegaraRai <SegaraRai@users.noreply.github.com>
2020-08-07 17:52:43 +0900

    RegManの宣言と定義を分離

--- tlg-wic-codec/wicx/RegMan.cpp
+++ tlg-wic-codec/wicx/RegMan.cpp
@@ -1,12 +1,26 @@
 ﻿#include "RegMan.hpp"
 
 namespace wicx {
+  void RegMan::SetSZ(wchar_t const* keyName, wchar_t const* valueName, LPCWSTR value) {
+    SetRaw(keyName, valueName, REG_SZ, value, sizeof(wchar_t) * (wcslen(value)));
+  }
+
+  void RegMan::SetDW(wchar_t const* keyName, wchar_t const* valueName, DWORD value) {
+    SetRaw(keyName, valueName, REG_DWORD, &value, sizeof(value));
+  }
+
+  void RegMan::SetBytes(wchar_t const* keyName, wchar_t const* valueName, void const* value, size_t count) {
+    SetRaw(keyName, valueName, REG_BINARY, value, count);
+  }
+
   void RegMan::SetRaw(wchar_t const* keyName, wchar_t const* valueName, unsigned type, void const* value, size_t valueSize) {
     m_keys.push_back(keyName);
+
     HKEY hKey;
     long err = RegCreateKeyW(HKEY_CLASSES_ROOT, keyName, &hKey);
-    if (err == ERROR_SUCCESS)
+    if (err == ERROR_SUCCESS) {
       err = RegSetValueExW(hKey, valueName, 0, type, reinterpret_cast<BYTE const*>(value), static_cast<DWORD>(valueSize));
+    }
   }
 
   void RegMan::Unregister() {
--- tlg-wic-codec/wicx/RegMan.hpp
+++ tlg-wic-codec/wicx/RegMan.hpp
@@ -4,24 +4,15 @@
 
 namespace wicx {
   class RegMan {
-  public:
-    void SetSZ(wchar_t const* keyName, wchar_t const* valueName, LPCWSTR value) {
-      SetRaw(keyName, valueName, REG_SZ, value, sizeof(wchar_t) * (wcslen(value)));
-    }
-
-    void SetDW(wchar_t const* keyName, wchar_t const* valueName, DWORD value) {
-      SetRaw(keyName, valueName, REG_DWORD, &value, sizeof(value));
-    }
-
-    void SetBytes(wchar_t const* keyName, wchar_t const* valueName, void const* value, size_t count) {
-      SetRaw(keyName, valueName, REG_BINARY, value, count);
-    }
-
-    void Unregister();
-
-  private:
     std::vector<wchar_t const*> m_keys;
 
     void SetRaw(wchar_t const* keyName, wchar_t const* valueName, unsigned type, void const* value, size_t valueSize);
+
+  public:
+    void SetSZ(wchar_t const* keyName, wchar_t const* valueName, LPCWSTR value);
+    void SetDW(wchar_t const* keyName, wchar_t const* valueName, DWORD value);
+    void SetBytes(wchar_t const* keyName, wchar_t const* valueName, void const* value, size_t count);
+
+    void Unregister();
   };
 } // namespace wicx
